29dc6858c428019c07db6721d58ed3c94275e5d2
Dylan Downard <dyldownard@outlook.com>
2017-10-10 18:53:12 -0400

    Added UML Diagrams, started Javadocs

--- Downard CSE248 Parking Garage/src/basePackage/CarsArray.java
+++ Downard CSE248 Parking Garage/src/basePackage/CarsArray.java
@@ -5,20 +5,36 @@ import java.io.Serializable;
 import carsPackage.*;
 import guiApplication.GUIFloor;
 
+/**
+ * @author Dylan
+ * @author https://github.com/battlebutts
+ *	@version 1.0
+ * 
+ */
+
 public class CarsArray implements Serializable{
 
 
-	/**
-	 * 
-	 */
+
 	private static final long serialVersionUID = -5068452776469704527L;
 	private Car[] aR;
 	protected int amountCars;
 	protected int amountSpaces;
 	private ParkingGarage mygarage;
+	/**
+	 * aR is an Array of Car objects
+	 * amountCars represents the amount of car elements in the array
+	 * amountSpaces represents empty spaces in the array
+	 * mygarage represents the ParkingGarage parent object
+	 */
 	
 	//--------------------------------------------------------	
 	
+	/**
+	 * Constructor
+	 * @param amount Size of array
+	 * @param mygarage Parent ParkingGarage
+	 */
 	public CarsArray(int amount, ParkingGarage mygarage) {
 		this.mygarage = mygarage;
 		aR = new Car[amount];
@@ -28,6 +44,13 @@ public class CarsArray implements Serializable{
 	
 	//--------------------------------------------------------	
 
+	/**
+	 * Park Car via Spot
+	 * @param myCar Car being parked
+	 * @param floor Parent floor
+	 * @param space Space car is going into
+	 * 
+	 */
 	public String parkCar(Car myCar, GUIFloor floor, int space) {
 		System.out.println(space + "space");
 		aR[space] = myCar;
@@ -39,6 +62,12 @@ public class CarsArray implements Serializable{
 		return "Car Parked.";
 	}
 	
+	/**
+	 * Park Car via Valet
+	 * @param myCar Car being parked
+	 * @param park Parent ParkingGarage
+	 * 
+	 */
 	public String parkValet(Car myCar, ParkingGarage park) {
 		this.mygarage = park;
 		int temp = 0;			//making sure that it gets the first slot available
@@ -59,6 +88,11 @@ public class CarsArray implements Serializable{
 	
 	//--------------------------------------------------------	
 	
+	/**
+	 * Car being Picked Up
+	 * @param myCar Car being picked up
+	 * @param spotnum Spot Car is being picked from
+	 */
 	public void CarPicked(Car myCar, int spotnum) {
 		amountSpaces++;
 		amountCars--;
@@ -71,6 +105,10 @@ public class CarsArray implements Serializable{
 
 	//--------------------------------------------------------	
 	
+	/** 
+	 *  Adds car to Parent ParkingGarage lot
+	 * @param myCar Car being added to global lot
+	 */
 	public void AddCarLot(Car myCar) {		// used in global
 		int temp = 0;			//making sure that it gets the first slot available
 		for (int i = 0; i < aR.length; i++) {
@@ -88,6 +126,11 @@ public class CarsArray implements Serializable{
 	
 	//--------------------------------------------------------	
 	
+	/**
+	 * Searched car array for ticket
+	 * @param tick Ticket to search for
+	 * 
+	 */
 	public Car searchTicket(String tick) {
 		for (int i = 0; i < aR.length; i++) {
 			if (aR[i] == null) {
@@ -101,6 +144,11 @@ public class CarsArray implements Serializable{
 	}
 	
 	
+	/**
+	 * Searches through cars for plate#
+	 * @param String plate Plate String to search for
+	 * 
+	 */
 	public Car searchPlate(String plate) {
 		for (int i = 0; i < aR.length; i++) {
 			if (aR[i] == null) {
@@ -115,10 +163,18 @@ public class CarsArray implements Serializable{
 	
 	//--------------------------------------------------------	
 	
+	/**
+	 * Grabs Array
+	 * @return Car Array
+	 */
 	public Car[] getAr() {
 		return aR;
 	}
 	
+	/**
+	 * Is array full
+	 * @return isFull
+	 */
 	public boolean isFull() {
 		return amountSpaces == 0;
 	}
